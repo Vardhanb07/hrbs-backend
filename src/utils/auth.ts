2f8b225b99263fcdbb3adb612a53da86e94df04b
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2026-02-05 20:02:27 +0530

    Add origin for better auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,12 +1,14 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../db/index.js";
+import { db } from "@/src/db/index.js";
+import { config } from "@/src/config";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: "pg",
+    provider: "pg"
   }),
   emailAndPassword: {
     enabled: true,
   },
+  trustedOrigins: [config.cors.origin],
 });
